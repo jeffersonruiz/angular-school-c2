4d594fd6de61a039e6abb2575a2187056f9dbf87
jefferson <jefferson.farid@hotmail.com>
2020-03-22 16:57:25 -0500

    clase 49 22/03/2020

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,20 +1,19 @@
-import { Directive, Input, ElementRef, OnChanges } from '@angular/core';
+import { Directive, Input, OnChanges, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective implements OnChanges {
-
-  @Input('appHighlight') isHighlighted:boolean = false;
+export class HighlightDirective implements OnChanges{
   
-  constructor(private element:ElementRef) {  }
+  @Input('appHighlight') isHighlight:boolean = false;
+  constructor(private element:ElementRef) { }
 
-  ngOnChanges(){
-    if(this.isHighlighted){
-      this.element.nativeElement.childNodes[0].style.backgroundColor = '#fdf498'; 
-    }else{
-      this.element.nativeElement.childNodes[0].style.backgroundColor = ''; 
-    }   
+  ngOnChanges() {
+    if (this.isHighlight) {
+      this.element.nativeElement.childNodes[0].style.backgroundColor = '#ffd498';
+    } else {
+      this.element.nativeElement.childNodes[0].style.backgroundColor = '';
+    }
   }
 
 }
